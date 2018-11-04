ff6187c8419c2ba959423e5620deb07beb8baecc
Limon <fengninglong@gmail.com>
2018-11-04 11:10:33 +0800

    swift 4.2

--- God/AppDelegate.swift
+++ God/AppDelegate.swift
@@ -15,14 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         Mediator.shared.coolie = self
 
         return true
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         let result = Mediator.shared.performAction(with: url)
         return result != nil
     }
--- God/ViewController.swift
+++ God/ViewController.swift
@@ -41,14 +41,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction private func test(_ sender: UIButton) {
+
         let callbackAction: ([String: Any]) -> Void = { info in
             print("Login callbacked with info: \(info)")
         }
 
-        let login = Mediator.shared.loginViewController(with: UIColor.red, callbackAction: callbackAction)!
-        navigationController?.pushViewController(login, animated: true)
-
-        print("Did Login: \(Mediator.shared.didLogin())")
+        if let login = Mediator.shared.loginViewController(with: UIColor.red, callbackAction: callbackAction) {
+            navigationController?.pushViewController(login, animated: true)
+            print("Did Login: \(Mediator.shared.didLogin())")
+        }
     }
 
     @IBAction private func testURL(_ sender: UIButton) {
@@ -60,18 +61,18 @@ class ViewController: UIViewController {
         /// scheme://[user]:[password]@[target]/[action]?[params]
         let testString = "God://Limon:123456@Target-Login/actionA?id=1234&page=2&name=egg"
 
-        let test = Mediator.shared.performAction(with: URL(string: testString)!) as? UIViewController
-
-        navigationController?.pushViewController(test!, animated: true)
+        if let test = Mediator.shared.performAction(with: URL(string: testString)!) as? UIViewController {
+            navigationController?.pushViewController(test, animated: true)
+        }
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        let parent = Parent()
-
-        let result = parent.test("parentSelector:", "coffee") // Works
-        print(result as! [String: Any])
+//        let parent = Parent()
+//
+//        let result = parent.test("parentSelector:", "coffee") // Works
+//        print(result as! [String: Any])
 
         // let user = parent.test("childSelector:", "apple") // No Works
         // print(user as! User)
